12905ff0a7263398c8a4c543e8973e807f6c1d47
ungros <82379236+ungros@users.noreply.github.com>
2023-04-24 20:20:53 +0300

    New Data

--- Weather/Service/URLService 2.0.swift
+++ Weather/Service/URLService 2.0.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-final class APIService {
+public class APIService {
     
-    static let shared = APIService()
+    public static let shared = APIService()
     
-    enum APIError: Error {
+    public enum APIError: Error {
         case error(_ errorString: String)
     }
     
-    func getJSON(urlString: String,
-                 complition: @escaping (Result<WeatherData, APIError>) -> Void) {
+    public func getJSON<T: Decodable>(urlString: String,
+                                      dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
+                                      keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+                                      complition: @escaping (Result<T, APIError>) -> Void) {
         
         guard let url = URL(string: urlString) else {
             complition(.failure(.error(NSLocalizedString("Error: Invalid URL", comment: ""))))
@@ -24,6 +26,7 @@ final class APIService {
         }
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request) { data, response, error in
+            
             if let error = error {
                 complition(.failure(.error("Error: \(error.localizedDescription)")))
                 return
@@ -33,8 +36,10 @@ final class APIService {
                 return
             }
             let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = dateDecodingStrategy
+            decoder.keyDecodingStrategy = keyDecodingStrategy
             do {
-                let decodedData = try decoder.decode(WeatherData.self, from: data)
+                let decodedData = try decoder.decode(T.self, from: data)
                 complition(.success(decodedData))
                 return
             } catch let decodingError {
@@ -42,6 +47,5 @@ final class APIService {
                 return
             }
         }.resume()
-        
     }
 }
--- Weather/WeatherData.swift
+++ Weather/WeatherData.swift
@@ -5,28 +5,45 @@
 //  Created by Roman on 19.04.2023.
 //
 
-import Foundation
-
-struct WeatherData: Codable {
-    let latitude, longitude, generationtimeMS: Double
-    let utcOffsetSeconds: Int
-    let timezone, timezoneAbbreviation: String
-    let elevation: Int
-    let currentWeather: CurrentWeather
-
-    enum CodingKeys: String, CodingKey {
-        case latitude, longitude
-        case generationtimeMS = "generationtime_ms"
-        case utcOffsetSeconds = "utc_offset_seconds"
-        case timezone
-        case timezoneAbbreviation = "timezone_abbreviation"
-        case elevation
-        case currentWeather = "current_weather"
-    }
-}
-
-struct CurrentWeather: Codable {
-    let temperature, windspeed: Double
-    let winddirection, weathercode: Int
-    let time: String
-}
+//import Foundation
+//
+//struct WeatherData: Decodable {
+//    let latitude, longitude, generationtimeMS: Double
+//    let utcOffsetSeconds: Int
+//    let timezone, timezoneAbbreviation: String
+//    let elevation: Int
+//    let currentWeather: CurrentWeather
+//    //let hourlyWeather: [HourlyWeather]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case latitude, longitude
+//        case generationtimeMS = "generationtime_ms"
+//        case utcOffsetSeconds = "utc_offset_seconds"
+//        case timezone
+//        case timezoneAbbreviation = "timezone_abbreviation"
+//        case elevation
+//        case currentWeather = "current_weather"
+//        //case hourly = "hourly"
+//    }
+//}
+//
+//struct CurrentWeather: Decodable {
+//    let temperature, windspeed: Double
+//    let winddirection, weathercode: Int
+//    let time: String
+//}
+//
+//struct HourlyWeather {
+//    let temperature, windspeed: Double
+//    let winddirection, weathercode: Int
+//    let time: String
+//}
+//
+////extension WeatherData {
+////    let urlString = "https://api.open-meteo.com/v1/forecast?latitude=34.90&longitude=56.19&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m_10m"
+////
+////    let apiService = APIService.shared
+////    apiService.getJSON(urlString: urlString) {(result: Result<WeatherData,APIService.APIError>) in
+////        <#code#>
+////    }
+////}
