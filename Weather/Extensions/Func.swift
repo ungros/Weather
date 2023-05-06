dc56444080aede52e6fd250c6f14e6e6a5b4169f
ungros <82379236+ungros@users.noreply.github.com>
2023-05-06 17:16:47 +0300

    Alert view

--- Weather/Extensions/Func.swift
+++ Weather/Extensions/Func.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 enum Extensions {
     
     enum Font {
--- Weather/UI/Segments/AllertCardView.swift
+++ Weather/UI/Segments/AllertCardView.swift
@@ -21,11 +21,22 @@ final class AllertCardView: UIView {
     private let allertLable: UILabel = {
         let lable = UILabel()
         lable.textColor = .black
-        lable.font = UIFont(name: <#T##String#>, size: <#T##CGFloat#>)
+        lable.font = Extensions.Font.NSK_B(with: 12)
+        lable.numberOfLines = 2
+        lable.text = "Cuaca esok hari kemungkinan akan terjadi hujan di siang hari"
         return lable
     }()
     
-    private let allertAnnotation: UILabel = {
+    private let allertLable2: UILabel = {
+        let lable = UILabel()
+        lable.textColor = UIColor(named: "Text")
+        lable.font = Extensions.Font.NSK(with: 12)
+        lable.numberOfLines = 2
+        lable.text = "Jangan lupa bawa payung ya"
+        return lable
+    }()
+    
+    private let allertAnnotation: UIImageView = {
         let image = UIImageView(image: UIImage(systemName: "cloud.bolt.fill"))
         image.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: 66, height: 66))
         image.backgroundColor = .clear
@@ -36,11 +47,9 @@ final class AllertCardView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
+        backgroundColor = UIColor(named: "Attention")
+        layer.cornerRadius = 12
         
-       
-       backgroundColor = UIColor(named: "Attention")
-       layer.cornerRadius = 12
-     
     }
     
     required init?(coder: NSCoder) {
@@ -48,21 +57,28 @@ final class AllertCardView: UIView {
     }
 }
 
-private extension AllertCardView {
-    
+private extension AllertCardView {    
     func setup() {
-        
-        
-        
-        
-        
-        
-        
-        
         addSubview(allertImage)
         allertImage.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.left.equalToSuperview().inset(16)
+        }
+        
+        addSubview(allertLable)
+        allertLable.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(64)
+            make.top.equalToSuperview().inset(16)
+            make.right.equalToSuperview().inset(93)
+            make.bottom.equalToSuperview().inset(36)
+        }
+        
+        addSubview(allertLable2)
+        allertLable2.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(64)
+            make.top.equalToSuperview().inset(54)
+            make.right.equalToSuperview().inset(93)
+            
         }
     }
 }
